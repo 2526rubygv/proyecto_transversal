673caea30a06c43fe53103fb0e90c75aaa949ef6
2526rubygv <2526_ruby.gonzales@iticbcn.cat>
2026-02-01 19:48:35 +0100

    cambio_2

--- script_DDL.sql
+++ script_DDL.sql
@@ -2,8 +2,8 @@ CREATE TABLE empresa (
 id_empresa INT PRIMARY KEY ,
 nom VARCHAR (50) NOT NULL,
 sector VARCHAR (50),
-contacte VARCHAR (50)
-ubicació VARCHAR (80) 
+contacte VARCHAR (50) ,
+ubicacio VARCHAR (80) ,
 modalitat_practica VARCHAR (50)
 );
 
