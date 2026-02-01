fb65bb1003f027ffe45e7dc9e5a2ab1f967fc013
2526rubygv <2526_ruby.gonzales@iticbcn.cat>
2026-02-01 18:41:10 +0100

    CTable_empresa

--- script_DDL.sql
+++ script_DDL.sql
@@ -1,1 +1,8 @@
-scripddl
+CREATE TABLE empresa (
+id_empresa INT PRIMARY KEY ,
+nom VARCHAR (50) NOT NULL,
+sector VARCHAR (50),
+contacte VARCHAR (50)
+ubicació VARCHAR (80) 
+modalitat_practica VARCHAR (50)
+);
